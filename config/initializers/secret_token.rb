0342bc2e0cb47cc9ceb0ef75c91c6e5345b7bfae
tannv85 <tannv85.@gmail.com>
2012-09-30 22:31:25 -0400

    change add gem
    Signed-off-by: tannv85 <tannv85.@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogs::Application.config.secret_token = 'b490b1c76ef564d76b0d97661f55d7c501404e9039210e5dbbc5cb5dc0ce0f2b3e5267350f630f4789228d5a53db7a8dbfabdf83e31362b52d329413038eed7a'
